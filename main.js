c56aa1bd48f3eb8ddaa9ea74d1eac6e7227fbb51
Ian <Ian.f.becker@gmail.com>
2026-05-21 11:48:43 -0500

    Final testing??

--- main.js
+++ main.js
@@ -25,3 +25,80 @@
 // ● Determine the largest and smallest values from [3, 78, -12,
 // 0.5, 27].
 // ● Generate a random number between 1 and 50.
+
+// absolute value
+function absoluteValue(num) {
+	result = Math.abs(num);
+	console.log(`The absolute value of ${num} is ${result}`);
+	return result;
+}
+
+// power
+function power(base, exponent) {
+	result = Math.pow(base, exponent);
+	console.log(`${base} to the power of ${exponent} is ${result}`);
+	return result;
+}
+
+// square root
+function squareRoot(num) {
+	result = Math.sqrt(num);
+	console.log(`The square root of ${num} is ${result}`);
+	return result;
+}
+
+function maxNum(numbers) {
+	result = Math.max(...numbers);
+	console.log(`The max is ${result}`);
+	return result;
+}
+
+function minNum(numbers) {
+	result = Math.min(...numbers);
+	console.log(`The min is ${result}`);
+	return result;
+}
+
+function randomNum(max) {
+	result = Math.floor(Math.random() * max);
+	console.log(`Your random number is ${result}`);
+	return result;
+}
+
+function roundNum(num) {
+	result = Math.round(num * 100) / 100;
+	console.log(`${num} rounded is ${result}`);
+	return result;
+}
+
+/* -------------------------------------------------------------------------- */
+// testing
+
+function calculator(operation, ...args) {
+	switch (operation) {
+		case "absoluteValue":
+			return absoluteValue(...args);
+		case "power":
+			return power(...args);
+		case "squareRoot":
+			return squareRoot(...args);
+		case "maxNum":
+			return maxNum(...args);
+		case "minNum":
+			return minNum(...args);
+		case "randomNum":
+			return randomNum(...args);
+		case "roundNum":
+			return roundNum(...args);
+		default:
+			return console.log("Not a valid operation!");
+	}
+}
+
+calculator("absoluteValue", -45.67);
+calculator("power", 5, 3);
+calculator("squareRoot", 144);
+calculator("maxNum", [3, 78, -12, 0.5, 27]);
+calculator("minNum", [3, 78, -12, 0.5, 27]);
+calculator("randomNum", 50);
+calculator("roundNum", 23.67891);
